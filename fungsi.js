8caabb0fe03ff7f85d86fc78b554eb201ac11fb1
Sandy Pratama <sandypratama25bisnis@gmail.com>
2024-06-03 21:30:30 +0700

    menghapus data yang tidak perlu

--- fungsi.js
+++ fungsi.js
@@ -1,6 +1,7 @@
-console.log('fungsi pertama');
-
 const add = (a, b) => a + b;
 console.log(add(1,2));
+
+const substract = (a, b) => a - b;
+console.log(add(5,2));
 
 console.log('test 2 baru');
--- fungsi2.js
+++
@@ -1,5 +0,0 @@
-console.log('ini file test')
-
-const deduct = (a, b) => a - b;
-
-console.log('test 2 baru');
